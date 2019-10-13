540c25dec80d36d4edbc3df4c74b636bf23b2e02
Melanie Simet <melanie.simet@gmail.com>
2019-10-12 20:55:13 -0500

    Lint process_hugos_data.py

--- analysis/process_hugos_data.py
+++ analysis/process_hugos_data.py
@@ -1,15 +1,25 @@
-import pickle
+"""
+process_hugos_data.py: Pull best novel data from a Hugo Awards html file and from a .txt file prepared by hand,
+and combine it into one dataset of all publicly available winners, nominees, and longlist nominees.
+"""
 import re
 import glob
 from bs4 import BeautifulSoup
 import pandas as pd
-import numpy as np
-import difflib
-
-""" Pull best novel data from a Hugo Awards html file."""
 
 def parse_html(filename):
-    year = re.search('(\d\d\d\d)', filename)
+    """
+    Process an HTML file of Hugo winners and nominees.
+
+    Parameters
+    ==========
+    filename: str
+        The filename to process
+    Return
+    =======
+    A list of dicts, one dict per winner or nominee
+    """
+    year = re.search(r'(\d\d\d\d)', filename)
     year = year.group(1)
     results = []
     with open(filename) as f:
@@ -24,7 +34,7 @@ def parse_html(filename):
                 text = nominee.text.split('by')
                 if text[0] == 'No Award':
                     continue
-                if len(text)==1:
+                if len(text) == 1:
                     text = text[0].split(',')
                 title = text[0].strip()
                 if title[-1] == ',': # Some say "Ender's Game, by Orson Scott Card"
@@ -42,238 +52,236 @@ def parse_html(filename):
     return results
 
 def get_winners():
+    """ Grab all the HTML files listing the winners, and return a list of dicts of all the information
+        contained in each file. """
     html_files = glob.glob("hugos_data/*.html")
     res = []
     for file_name in html_files:
         res.extend(parse_html(file_name))
     return res
 
-""" Code to parse the Hugos PDF data I copied and pasted into a text file """
-
 def nip_percent(s):
-    res = re.search("(.*) \([\d.]+\%\)", s)
+    """
+    Some of the elements in the text file have percentages that mess up our assumed patterns. This nips that substring
+    from the end of the main book string.
+
+    Parameters
+    ==========
+    s: str
+        The line of text from the Hugos text file
+    Returns
+    =======
+    A string with any percent value removed from the end
+    """
+    res = re.search(r"(.*) \([\d.]+\%\)", s)
     if res:
         return res.group(1)
-    res = re.search("(.*) [\d.]+\%\s*$", s)
+    res = re.search(r"(.*) [\d.]+\%\s*$", s)
     if res:
         return res.group(1)
     return s
 
-# checked
 def t1(s):
-    # Section header
-    res = re.search('^(\d\d\d\d) (novel|novella) (nomination|vote)\s*$', s.lower())
+    """ regexp search for section header """
+    res = re.search(r'^(\d\d\d\d) (novel|novella) (nomination|vote)\s*$', s.lower())
     if not res:
         return None
     return {"year": res.group(1), "booktype": res.group(2), "datatype": res.group(3)+'s'}
 
-#checked
 def t2(s):
-    # No Award count
-    res = re.search("no award ([\d]+)\s*$", s.lower())
+    """ regexp search for No Award count """
+    res = re.search(r"no award ([\d]+)\s*$", s.lower())
     if not res:
         return None
     return {"Title": "No Award", "Author": "None", "Count": res.group(1)}
 
 def t2a(s):
+    """ regexp search for No Award """
     res = re.search("no award", s.lower())
     if not res:
         return None
     return {"Title": "No Award", "Author": "None"}
 
-# checked
 def t3(s):
-    # Multiple numbers
-    res = re.search("^([\d]+)\s+(?:\d+\s+)*\d+\s*$", s)
+    """ Regexp search for multiple numbers """
+    res = re.search(r"^([\d]+)\s+(?:\d+\s+)*\d+\s*$", s)
     if not res:
         return None
     return {"Count": res.group(1)}
 
-#checked
 def t4(s):
-    # Count Authorlast, Authorfirst. Title (Publisher)
-    res = re.search("^([\d]+) (.*?), (.*)[.] (.*?) \(.*?\)\s*$", s)
+    """ regexp search for Count Authorlast, Authorfirst. Title (Publisher) """
+    res = re.search(r"^([\d]+) (.*?), (.*)[.] (.*?) \(.*?\)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(4), "Author": "{} {}".format(res.group(3), res.group(2)), "Count": res.group(1)}
 
-#checked
 def t5(s):
-    # Title, by Author - Count
-    res = re.search("(.*?), by (.*?) (?:-|–) ([\d]+)\s*$", s)
+    """ regexp search for Title, by Author - Count """
+    res = re.search(r"(.*?), by (.*?) (?:-|–) ([\d]+)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(1), "Author": res.group(2), "Count": res.group(3)}
 
-# checked
 def t6(s):
-    # Count Title by Author (Publisher)
-    res = re.search("^([\d]+) (.*?) by (.*?) \(.*?\)\s*$", s)
+    """ regexp search for Count Title by Author (Publisher) """
+    res = re.search(r"^([\d]+) (.*?) by (.*?) \(.*?\)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(2), "Author": res.group(3), "Count": res.group(1)}
 
-#checked
 def t7(s):
-    # Title by Author Count
-    res = re.search("(.*?) by (.*?) ([\d]+)\s*$", s)
+    """ regexp search for Title by Author Count """
+    res = re.search(r"(.*?) by (.*?) ([\d]+)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(1), "Author": res.group(2), "Count": res.group(3)}
 
-#checked
 def t8(s):
-    # Title - Author - Count
-    res = re.search("(.*?) (?:-|–) (.*?) (?:-|–) ([\d]+)\s*$", s)
+    """ regexp search for Title - Author - Count """
+    res = re.search(r"(.*?) (?:-|–) (.*?) (?:-|–) ([\d]+)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(1), "Author": res.group(2), "Count": res.group(3)}
 
-#checked
 def t9(s):
-    # Count Title (Authorlast) Nominationcount
-    res = re.search("^([\d]+) (.*?) \((.*?)\) ([\d]+)\s*$", s)
+    """ regexp search for Count Title (Authorlast) Nominationcount """
+    res = re.search(r"^([\d]+) (.*?) \((.*?)\) ([\d]+)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(2), "Author": res.group(3), "NominationCount": res.group(4), "Count": res.group(1)}
 
-# Checked
 def t10(s):
-    # Count Title (Author; Publisher)
-    res = re.search("^([\d]+)\s+(.*?) \((.*?); .*?\)\s*$", s)
+    """ regexp search for Count Title (Author; Publisher) """
+    res = re.search(r"^([\d]+)\s+(.*?) \((.*?); .*?\)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(2), "Author": res.group(3), "Count": res.group(1)}
 
-# checked
 def t11(s):
-    # Count Title by Author
-    res = re.search("^([\d]+)(.*?)by(.*)", s)
+    """ regexp search for Count Title by Author """
+    res = re.search(r"^([\d]+)(.*?)by(.*)", s)
     if not res:
         return None
     return {"Title": res.group(2).strip(), "Author": res.group(3).strip(), "Count": res.group(1)}
 
-#checked
 def t12(s):
-    # Count Count2 TitleAuthor
-    res = re.search("^(\d+) [\d.]+ (.*)", s)
+    """ regexp search for Count Count2 TitleAuthor """
+    res = re.search(r"^(\d+) [\d.]+ (.*)", s)
     if not res:
         return None
     return {"TitleAuthor": res.group(2), "Count": res.group(1)}
 
-# checked
 def t13(s):
-    # Title Count
-    res = re.search("(.*?) ([\d]+)\s*$", s)
+    """ regexp search for Title Count """
+    res = re.search(r"(.*?) ([\d]+)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(1), "Count": res.group(2)}
 
-# checked
 def t14(s):
-    # Count
-    res = re.search("^([\d]+)\s*$", s)
+    """ regexp search for Count """
+    res = re.search(r"^([\d]+)\s*$", s)
     if not res:
         return None
     return {"Count": res.group(1)}
 
-#checked
 def t15(s):
-    # (count) Title
-    res = re.search("^\(([\d]+)\) (.*?)$", s)
+    """ regexp search for (count) Title """
+    res = re.search(r"^\(([\d]+)\) (.*?)$", s)
     if not res:
         return None
     return {"Title": res.group(2), "Count": res.group(1)}
 
-#checked
 def t16(s):
-    # (Count)
-    res = re.search("^\(([\d]+)\)\s*$", s)
+    """ regexp search for (Count) """
+    res = re.search(r"^\(([\d]+)\)\s*$", s)
     if not res:
         return None
     return {"Count": res.group(1)}
 
-# checked
 def t17(s):
-    # Count Title, Author
-    res = re.search("^([\d]+)(.*), (.*)", s)
+    """ regexp search for Count Title, Author """
+    res = re.search(r"^([\d]+)(.*), (.*)", s)
     if not res:
         return None
     return {"Title": res.group(2).strip(), "Author": res.group(3).strip(), "Count": res.group(1)}
 
-# checked
 def t18(s):
-    # Count\tTitle\tAuthor
-    res = re.search("^([\d]+)\s*\t(.*)\t(.*)", s)
+    """ regexp search for Count\tTitle\tAuthor """
+    res = re.search(r"^([\d]+)\s*\t(.*)\t(.*)", s)
     if not res:
         return None
     return {"Title": res.group(2).strip(), "Author": res.group(3).strip(), "Count": res.group(1)}
 
-# checked
 def t19(s):
-    # Title\tby\tAuthor\tCount
-    res = re.search("(.*)\tby\t(.*?)\t([\d]+)\s*$", s)
+    """ regexp search for Title\tby\tAuthor\tCount """
+    res = re.search(r"(.*)\tby\t(.*?)\t([\d]+)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(1).strip(), "Author": res.group(2).strip(), "Count": res.group(3)}
 
-#checked
 def t20(s):
-    # Count Title
-    res = re.search("^([\d]+) (.*?)$", s)
+    """ regexp search for Count Title """
+    res = re.search(r"^([\d]+) (.*?)$", s)
     if not res:
         return None
     return {"Title": res.group(2), "Count": res.group(1)}
 
-# checked
 def t21(s):
-    # Title by Author (Publisher)
-    res = re.search("(.*)\s*by\s*(.*?) \(.*?\)\s*$", s)
+    """ regexp search for Title by Author (Publisher) """
+    res = re.search(r"(.*)\s*by\s*(.*?) \(.*?\)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(1).strip(), "Author": res.group(2)}
 
-#checked
 def t22(s):
-    # Title by Author
-    res = re.search("(.*?) by (.*?)$", s)
+    """ regexp search for Title by Author """
+    res = re.search(r"(.*?) by (.*?)$", s)
     if not res:
         return None
     return {"Title": res.group(1), "Author": res.group(2).strip()}
 
-#checked
 def t23(s):
-    # Title (Author; Publisher)
-    res = re.search("(.*?) \((.*?); .*?\)\s*$", s)
+    """ regexp search for Title (Author; Publisher) """
+    res = re.search(r"(.*?) \((.*?); .*?\)\s*$", s)
     if not res:
         return None
     return {"Title": res.group(1), "Author": res.group(2)}
 
-#checked
 def t24(s):
-    # Authorlast: Title
-    res = re.search("^(.*?): (.*?)$", s)
+    """ regexp search for Authorlast: Title """
+    res = re.search(r"^(.*?): (.*?)$", s)
     if not res:
         return None
     return {"Title": res.group(2), "Author": res.group(1)}
 
-#checked
 def t25(s):
-    # Title, Author
-    res = re.search("(.*), (.*)", s)
+    """ regexp search for Title, Author """
+    res = re.search(r"(.*), (.*)", s)
     if not res:
         return None
     return {"Title": res.group(1), "Author": res.group(2)}
 
-#checked
 def t26(s):
-    # Title
+    """ Simply return title, if no other pattern has worked """
     return {"Title": s.strip().title()}
 
 def process_textfile():
+    """
+    Process the Hugos voting data text file produced from PDFs. Essentially, turn all the strings into dicts of
+    year, author, title, and vote or nominee count (not all keys will be populated for any given dict, given the
+    structure of the files).
+    """
     with open('hugos_data/hugos_voting_data.txt') as f:
         data = f.readlines()
     res = []
+    # There are a huge number of patterns that the lines in the text file may follow. This extremely long statement
+    # checks them from most complex to least complex, returning the results for the most complex pattern that matches
+    # the string. I.e., 25 King, Stephen. Cujo would return {"Count": 25, "Author": "Stephen King", "Title": "Cujo"}
+    # and not the string itself; only strings that do not match any pattern will be returned as-is (to handle titles
+    # on single lines).  So step through each pattern. If it doesn't match it returns None, so move onto the next
+    # pattern. Otherwise, add the new dict onto the `res` list and continue to the next string.
     for s in data:
         s = nip_percent(s)
         for _ in range(1):
@@ -384,19 +392,25 @@ def process_textfile():
             res.append(t26(s))
     return res
 
-def list_get(list, i, default):
-    if len(list)>i:
-        return list[i]
+def list_get(l, i, default):
+    """ Produce a .get method for lists that works like the .get method for dicts.
+        We need this because we are, essentially, zipping lists together, but we need the max length not the
+        min length of any string, and we're not actually zipping anything because we're making row dicts for a
+        DataFrame."""
+    if len(l) > i:
+        return l[i]
     return default
 
 def main():
+    """ Parse all the data in HTML and text files describing the Hugos winners, and produce a giant dataframe
+        that contains all of the relevant information."""
     winners = get_winners()
     votes = process_textfile()
 
     voting_data = {}
     year = None
     booktype = None
-    for i in range(1989,2020):
+    for i in range(1989, 2020):
         voting_data[str(i)] = {}
         for datatype in ['votes', 'nominations']:
             voting_data[str(i)][datatype] = {}
@@ -441,37 +455,12 @@ def main():
                     continue
                 for i in range(len(this_vd['Title'])):
                     df = df.append({key: list_get(this_vd[key], i, None) for key in this_vd}, ignore_index=True)
-                if False:
-                    scores = np.zeros(len(df))
-                    for booktype in ['novel', 'novella']:
-                        bmask = df['BookType'] == booktype
-                        winner_mask = (df['Winner']=='W')
-                        nominee_mask = ((df['Winner']=='N') | winner_mask)
-                        df = df.astype({'VoteCount': float, 'NominationCount': float, 'Score': float, 'Year': int})
-                        if np.any(~(df['VoteCount']).isna()):
-                            try:
-                                win_votecount = df['VoteCount'][winner_mask & bmask].values[0]
-                            except IndexError:
-                                if year == 2015:
-                                    win_votecount = 3495 # Noah Ward!
-                            scores[nominee_mask & bmask] = 1.0*df['VoteCount'][nominee_mask & bmask].values/win_votecount
-                            if np.any(~np.isnan(df['NominationCount'])):
-                                nominee_minscore = scores[nominee_mask & bmask].min()
-                                nominee_nomcount = df['NominationCount'][nominee_mask & bmask].values.min()
-                                scores[bmask & ~nominee_mask] = nominee_minscore*df['NominationCount'][bmask & ~nominee_mask]/nominee_nomcount
-                            if year == 2015:
-                                print("Pause here")
-                        else:
-                            scores[nominee_mask & bmask] = 0.7
-                            scores[winner_mask & bmask] = 1
-                    df['Score'] = scores
                 list_of_dfs.append(df)
 
 
     df = pd.concat(list_of_dfs, ignore_index=True)
     df = df.drop_duplicates()
     df.to_csv('hugos_data/hugo_votecounts_raw.csv')
-#    print(df)
 
     df = pd.DataFrame(columns=['Author', 'Title', 'Year', 'Winner'])
     for w in winners:
@@ -479,5 +468,5 @@ def main():
 
     df.to_csv('hugos_data/winners.csv')
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
